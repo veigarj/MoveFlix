223845412a801d1289ac8d5314c9e30101794080
Glauco Veiga <veiga.cg@gmail.com>
2023-04-10 21:00:11 -0300

    SearchParams

--- MovieFlix/src/components/MovieCard.jsx
+++ MovieFlix/src/components/MovieCard.jsx
@@ -9,10 +9,12 @@ const MovieCard = ({ movie, showLink = true }) => {
     <div className="movie-card">
       <img src={imagesURL + movie.poster_path} alt={movie.title} />
       <h2>{movie.title}</h2>
-      <p><FaStar />{movie.vote_average} </p>
+      <p>
+        <FaStar /> {movie.vote_average}
+      </p>
       {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
     </div>
-  )
-}
+  );
+};
 
-export default MovieCard
+export default MovieCard;
--- MovieFlix/src/components/Navbar.jsx
+++ MovieFlix/src/components/Navbar.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {BiCameraMovie, BiSearchAlt2} from 'react-icons/bi'
+import { Link, useNavigate } from 'react-router-dom'
 
 import './Navbar.css'
 
-import { Link } from 'react-router-dom'
-
 const Navbar = () => {
-  return (
-    <div>
-       <nav id='navbar'>
-        <h2>
-          <Link to='/'><BiCameraMovie /> MovieFlix</Link>
-        </h2>
-          <form action="">
-            <input type="text" placeholder='Busque um filme' />
-            <button type='submit'><BiSearchAlt2/></button>
-          </form>
-      </nav>
-    </div>
-  )
-}
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
 
-export default Navbar
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    navigate(`/search?q=${search}`, { replace: true });
+    setSearch("");
+  };
+
+  return (
+    <nav id="navbar">
+      <h2>
+        <Link to="/">
+          <BiCameraMovie /> MoviesLib
+        </Link>
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Busque um filme"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+      </form>
+    </nav>
+  );
+};
+
+export default Navbar;
